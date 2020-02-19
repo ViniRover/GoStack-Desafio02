f7c10ab8eafb62eb2cd78327f2442c64defac6a2
ViniRover <viniciusrover3@gmail.com>
2020-02-19 14:33:59 -0300

    Etapa 02 - Upload de imagem, Deliveryman Controller

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,17 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import RecipientController from './app/controllers/RecipientController';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import DeliverymanController from './app/controllers/DeliverymanController';
 import authMidlleware from './app/middlewares/auth';
 
+import multerConfig from './config/multer';
+
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -13,6 +19,11 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMidlleware);
 routes.post('/recipient', RecipientController.store);
 routes.put('/recipient', RecipientController.update);
+
 routes.put('/users', UserController.update);
+
+routes.post('/deliverymans', DeliverymanController.store);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
