795a4c8c37207da96971d0a972cefedfb5cf4bc5
ViniRover <viniciusrover3@gmail.com>
2020-03-04 11:07:13 -0300

    Término do DeliverymanController

--- src/app/controllers/DeliverymanController.js
+++ src/app/controllers/DeliverymanController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import File from '../models/File';
 import Deliveryman from '../models/Deliveryman';
 
 class DeliverymanController {
@@ -32,7 +33,68 @@ class DeliverymanController {
   }
 
   async update(req, res) {
-    return res.json();
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { id } = req.params;
+
+    const deliveryman = await Deliveryman.findByPk(id);
+
+    const { email } = req.body;
+
+    if (email && email !== deliveryman.email) {
+      const deliverymanExist = await Deliveryman.findOne({ where: { email } });
+
+      if (deliverymanExist) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+    }
+
+    const { name } = await deliveryman.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
+  }
+
+  async index(req, res) {
+    const { page = 1 } = req.query;
+    const deliveryman = await Deliveryman.findAll({
+      attributes: ['id', 'name', 'email'],
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+      limit: 20,
+      offset: (page - 1) * 20,
+    });
+
+    return res.json(deliveryman);
+  }
+
+  async delete(req, res) {
+    const { id } = req.params;
+
+    const deliveryman = await Deliveryman.findByPk(id);
+
+    if (!deliveryman) {
+      return res.status(400).json({ error: 'Deliveryman does not exist' });
+    }
+
+    await deliveryman.destroy();
+
+    return res.json(deliveryman);
   }
 }
 
